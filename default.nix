a661ff62e0c4bd8eedaef1cdff49f50ae0813cfb
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-09-09 14:21:18 +0200

    NixCon 2023 draft

---
+++ default.nix
@@ -0,0 +1,29 @@
+{
+  dream2nixSource ?
+    builtins.fetchTarball {
+      url = "https://github.com/nix-community/dream2nix/tarball/main";
+      # sha256 = "";
+    },
+}: let
+  dream2nix = import dream2nixSource;
+  nixpkgs = import dream2nix.inputs.nixpkgs {};
+  # current pyproject project
+  packages = dream2nix.lib.importPackages {
+    projectRoot = ./.;
+    projectRootFile = "pyproject.toml";
+    packagesDir = "./nix";
+    # packagesList = [ "./package.nix" ];
+    packageSets.nixpkgs = nixpkgs;
+  };
+in
+  packages.someproject
+  
+  # dream2nix.lib.importPackage {
+  #   projectRoot = ./.;
+  #   projectRootFile = "pyproject.toml";
+  #   packageFile = "./package.nix";
+  #   packageSets.nixpkgs = nixpkgs;
+  # };
+
+  # TODO
+  # nix run -f default.nix lock
