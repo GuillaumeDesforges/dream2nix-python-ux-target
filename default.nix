4db0194604844cf82f35a5fbe04bb348005698f5
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-09-21 16:16:47 +0200

    it works

--- default.nix
+++ default.nix
@@ -1,12 +1,14 @@
 { dream2nixSource ? builtins.fetchTarball {
     url = "https://github.com/nix-community/dream2nix/tarball/0d3146539a0c57cbe9c49b8d3b1852d04394cec6";
-    # sha256 = "";
   }
-,
+, nixpkgsSource ? builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/tarball/00416be09afb1de1c4f91ff7f11da1a68b711899";
+  }
 }:
 let
   dream2nix = import dream2nixSource;
   nixpkgs = import dream2nix.inputs.nixpkgs { };
+  # nixpkgs = import nixpkgsSource { };
   # current pyproject project
   settings = {
     paths.projectRoot = ./.;
--- package.nix
+++ package.nix
@@ -10,19 +10,7 @@
 
   mkDerivation.src = ./.;
 
-  deps = { nixpkgs, ... }: {
-    inherit (nixpkgs.cudaPackages) autoAddOpenGLRunpathHook;
-  };
-
-  pip.drvs.torch.mkDerivation.nativeBuildInputs = [
-    config.deps.autoAddOpenGLRunpathHook
-  ];
-
-  pip.drvs.nvidia-curand-cu11.env.cudaNeededPhase = ''
-    patchelf --add-needed libcuda.so.1 $out/${config.deps.python.sitePackages}/nvidia/*/lib/*
-  '';
-  pip.drvs.nvidia-curand-cu11.mkDerivation.postPhases = [ "cudaNeededPhase" ];
-  pip.drvs.nvidia-curand-cu11.mkDerivation.nativeBuildInputs = [
-    config.deps.autoAddOpenGLRunpathHook
-  ];
+  pip.drvs.torch.env.appendRunpaths = [ "/run/opengl-driver/lib" ];
+  pip.drvs.nvidia-cublas-cu11.env.appendRunpaths = [ "/run/opengl-driver/lib" ];
+  pip.drvs.nvidia-curand-cu11.env.appendRunpaths = [ "/run/opengl-driver/lib" ];
 }
