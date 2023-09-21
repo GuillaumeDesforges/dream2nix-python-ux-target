e59c0635c3adeea2b2dc9df856fde538ada5e681
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-09-21 14:38:20 +0200

    Add more CUDA patching, but doesn't work

--- default.nix
+++ default.nix
@@ -1,24 +1,24 @@
-{
-  dream2nixSource ?
-    builtins.fetchTarball {
-      url = "https://github.com/nix-community/dream2nix/tarball/main";
-      # sha256 = "";
-    },
-}: let
+{ dream2nixSource ? builtins.fetchTarball {
+    url = "https://github.com/nix-community/dream2nix/tarball/main";
+    # sha256 = "";
+  }
+,
+}:
+let
   dream2nix = import dream2nixSource;
-  nixpkgs = import dream2nix.inputs.nixpkgs {};
+  nixpkgs = import dream2nix.inputs.nixpkgs { };
   # current pyproject project
   settings = {
-    paths.projectRoot = ./.;
-    paths.projectRootFile = "pyproject.toml";
-    paths.package = ".";
+    paths.projectRoot = "${./.}";
+    paths.projectRootFile = "${./pyproject.toml}";
+    paths.package = "${./.}";
   };
   package = dream2nix.lib.evalModules {
-    modules = [settings ./package.nix];
+    modules = [ settings ./package.nix ];
     packageSets.nixpkgs = nixpkgs;
   };
 in
-  package
+package
 
-  # TODO
-  # nix run -f default.nix lock
+# TODO
+# nix run -f default.nix lock
--- package.nix
+++ package.nix
@@ -10,10 +10,17 @@
 
   mkDerivation.src = ./.;
 
-  deps = {nixpkgs, ...}: {
+  deps = { nixpkgs, ... }: {
     inherit (nixpkgs.cudaPackages) autoAddOpenGLRunpathHook;
   };
   pip.drvs.torch.mkDerivation.nativeBuildInputs = [
+    config.deps.autoAddOpenGLRunpathHook
+  ];
+  pip.drvs.nvidia-curand-cu11.env.cudaNeededPhase = ''
+    patchelf --add-needed libcuda.so.1 $out/${config.deps.python.sitePackages}/nvidia/*/lib/*
+  '';
+  pip.drvs.nvidia-curand-cu11.mkDerivation.postPhases = [ "cudaNeededPhase" ];
+  pip.drvs.nvidia-curand-cu11.mkDerivation.nativeBuildInputs = [
     config.deps.autoAddOpenGLRunpathHook
   ];
 }
